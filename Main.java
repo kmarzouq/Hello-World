802f0f8808c0ae72a8db5a40bb97b14e8dd0be78
kmarzouq <69616884+kmarzouq@users.noreply.github.com>
2020-08-12 21:37:45 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I like playing video games.");
+		System.out.println("My favorite video games are Uncharted 4 and Freedom Wars");
+		System.out.println("I want to go to UC Berkeley ");
+		System.out.println("I want to major in Computer Science and Computer Programming");
+		System.out.println("I am really interested in AI");
+	}
+}
